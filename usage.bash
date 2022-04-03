2e991d235ed9bb78a3dd115dfa41610d7606be53
Adrien Jarretier-Yuste <jarretier.adrien@gmail.com>
2022-04-03 09:29:51 +0200

    removed file usage.bash

--- usage.bash
+++
@@ -1,9 +0,0 @@
-function usage()
-{
-    echo ''
-    echo "usage: bash make.bash <X.Y.Z> [-h | --help] [-c <fileType>]"
-    echo ""
-    echo "  -h --help"
-    echo -e "  -c <fileType> \t If option is given, compile files using make.<fileType>.bash"
-    echo ""
-}
