574d20d37111d69a8e81cafdc6ae6394448a86cd
Adrien Jarretier-Yuste <jarretier.adrien@gmail.com>
2022-04-03 09:59:15 +0200

    update command line usage help

--- make.bash
+++ make.bash
@@ -5,7 +5,7 @@ releasedName='releasedName'
 function usage()
 {
     echo ''
-    echo "usage: bash make.bash <X.Y.Z> [-h | --help] [-c <fileType>]"
+    echo "usage: bash make.bash <X.Y.Z> [-h | --help] [-c <fileType>] [--copy]"
     echo ""
     echo "  -h --help"
     echo -e "  -c <fileType> \t If option is given, compile files using make.<fileType>.bash"
